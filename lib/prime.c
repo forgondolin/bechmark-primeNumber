84b6072287b08121253b9f669ff561bce07905c3
kaleb <kaleb.lucas@aluno.ufabc.edu.br>
2019-11-03 22:35:50 -0300

    arquivos de performance

---
+++ lib/prime.c
@@ -0,0 +1,15 @@
+//checar número primo
+int isPrime(num)
+{
+        for(int i =2; i< num; i++)
+        if(num% i ==0) return 0;
+        return (num != 1 && num != 0) ? 1 : 0;
+}
+
+   int checkPrime(num) {
+    int count = 0;
+    for (int i = 0 ; i < num; i++){
+        if(isPrime(i)) count++;    
+    }    
+    return count;
+}
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(2222, () => console.log("rodando na porta 2222"))
+
